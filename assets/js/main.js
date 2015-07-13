e19149d558fd6142c7eed22b16ad0de615e3fc67
dj51234 <dj51234@yahoo.com>
2015-07-13 19:53:51 -0400

    gh-pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-	
+	$('.image').mouseenter(function() {
+		$('.overlay', this).css('opacity','1');
+	});
+	$('.image').mouseleave(function() {
+		$('.overlay', this).css('opacity','0');
+	});
+	$('a.gallery').featherlightGallery({
+    openSpeed: 300
+	});
 });
 
 
